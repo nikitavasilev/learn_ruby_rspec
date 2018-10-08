d18e19fe270a7c799f441662cbabab8cf0096123
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-10-08 22:32:36 +0200

    initial commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add(i, j)
+	result = i + j
+end
+
+def subtract(i, j)
+	result = i - j	
+end
+
+def sum(array)
+	array = array.sum
+end
+
+def multiply(i, j)
+	result = i * j
+end
+
+def power(i, j)
+	result = i ** j
+end
+
+def factorial(n)
+  if n == 0
+    return 1
+  else
+    n * factorial(n - 1)
+  end
+end
