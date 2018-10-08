0836dbf6bddfb300d4beec88b01d03199a62b3c4
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-10-08 22:31:17 +0200

    initial commit

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
-#write your code here
+def ftoc(fahrenheit)
+	celsius = ((fahrenheit - 32) / 1.8).round
+end
+
+def ctof(celsius)
+	fahrenheit = ((celsius * 1.8) + 32)
+end
+
+def temperature
+	ftoc(fahrenheit)
+	ctof(celsius)
+end
