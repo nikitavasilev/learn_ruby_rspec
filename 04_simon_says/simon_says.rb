b2328625ec3fb3e9ba0a664eef576e8bdec5bdef
Ysalien <enilasy.m@gmail.com>
2018-10-08 20:13:18 +0200

    nouvelle version exo 4

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
 #write your code here
+def echo(string)
+  "#{string}"
+  "#{string}"
+end
+
+def shout(string)
+  "#{string.upcase}"
+end
+
+def repeat(string, times = 2)
+  string = (string + " ")
+  string = (string * times).chop
+end
+
+def start_of_word(string, characters)
+  string = string.split(//).first(characters).join
+end
+
+def first_word(string)
+  string.split.first
+end
+
+def titleize(string)
+  string.split.map(&:capitalize).join(' ')
+end
