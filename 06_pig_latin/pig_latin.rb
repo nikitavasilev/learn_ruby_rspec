64bb105bb6b43b55131d9361a42addfd144b0058
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-10-09 00:02:32 +0200

    delete empty lines

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -4,10 +4,8 @@ def translate(s)
   consonants = alpha - vowels
   if vowels.include?(s[0])
     s + "ay"
-
   elsif consonants.include?(s[0]) && consonants.include?(s[1])
     s[2..-1] + s[0..1] + "ay"
-
   elsif consonants.include?(s[0])
     s[1..-1] + s[0] + "ay"
   end
